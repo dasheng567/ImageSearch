ffc5e9f8e1106f38343d4aee63a94b081a40d541
Chen Wang <w.chen9962@gmail.com>
2024-08-24 12:17:52 -0400

    Initial Commit

---
+++ ImageSearch/ImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearchApp.swift
+//  ImageSearch
+//
+//  Created by Chen Big on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
